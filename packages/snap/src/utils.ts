e4c67ca4f14dd02f6f8e48df919676c7f30f691b
Piotr Roslaniec <p.roslaniec@gmail.com>
2023-12-09 12:18:06 +0100

    address say-03

--- packages/snap/src/utils.ts
+++ packages/snap/src/utils.ts
@@ -1,16 +1,5 @@
 import SHA3 from 'sha3';
 
-export const RNG_SEED_SIZE = 32;
-
-export const getRandomBytes = (byteCount: number): Int32Array => {
-  if (!window.crypto?.getRandomValues) {
-    throw new Error('window.crypto.getRandomValues not available');
-  }
-  const randomBytes = new Int32Array(byteCount);
-  window.crypto.getRandomValues(randomBytes);
-  return randomBytes;
-};
-
 export const sha256 = (message: string): Uint8Array => {
   const hash = new SHA3(256);
   hash.update(message);
